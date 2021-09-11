46f76243f5ee7772881a703677042d3b5d61dc54
HaileyStorm <HaileyCollet@gmail.com>
2021-09-11 16:50:14 -0600

    Export InitRandom

--- math/random.go
+++ math/random.go
@@ -9,14 +9,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// initRandom seeds math.rand with crypto/rand (imported as cryptorand), such that future math.rand operations are more or less cryptographically
+// InitRandom seeds math.rand with crypto/rand (imported as cryptorand), such that future math.rand operations are more or less cryptographically
 // secure. It falls back to seeding with current nanosecond time. Without either, the math/rand package will always
 // initialize with the same seed (0, I think).
 // See: https://stackoverflow.com/a/54491783/5061881
 // Imports:
 // cryptorand "crypto/rand"
 // log "github.com/sirupsen/logrus"
-func initRandom() {
+func InitRandom() {
 	// Gets 8 bytes using the cryptographically secure random package, and casts them into a uint64 and then an int64
 	// (if you use a random byte for the most significant byte of a signed int64 you aren't randomly assigning the sign
 	// bit, thus the conversion to unsigned first). I believe it shouldn't matter whether you use LittleEndian or
