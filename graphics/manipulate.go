53a278d575fb6705c06459660085cec31b8ce1b9
HaileyStorm <HaileyCollet@gmail.com>
2021-09-18 12:41:24 -0600

    added graphics\manipulate.go, with image resizing methods.

---
+++ graphics/manipulate.go
@@ -0,0 +1,28 @@
+package graphics
+
+import (
+	"image"
+
+	"github.com/nfnt/resize"
+)
+
+// ResizeMaintain resizes img while maintaining its aspect ratio and ensuring that the new image fills the desired size.
+// The source image will be cut off in the smaller target dimension if they are not the same aspect ratio.
+// The upper-left corners are aligned.
+// This method uses the Nearest Neighbor interpolation algorithm. For other algorithms, use ResizeMaintainWithInterp.
+func ResizeMaintain(img image.Image, targetWidth, targetHeight uint) image.Image {
+	return ResizeMaintainWithInterp(img, targetWidth, targetHeight, resize.NearestNeighbor)
+}
+
+// ResizeMaintainWithInterp resizes img while maintaining its aspect ratio and ensuring that the new image fills the desired size.
+// The source image will be cut off in the smaller target dimension if they are not the same aspect ratio.
+// The upper-left corners are aligned.
+// The resize is performed using the interpolation algorithm provided by function. Note that resize.NearestNeighbor is
+// the fastest available algorithm, but will not always produce clean results.
+func ResizeMaintainWithInterp(img image.Image, targetWidth, targetHeight uint, function resize.InterpolationFunction) image.Image {
+	if targetWidth >= targetHeight && img.Bounds().Dx() >= img.Bounds().Dy() {
+		return resize.Resize(0, uint(targetHeight), img, function)
+	} else {
+		return resize.Resize(uint(targetWidth), 0, img, function)
+	}
+}
