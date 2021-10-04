e2f69c4093fd81d7232f32810fb3ed4912bc582b
HaileyStorm <HaileyCollet@gmail.com>
2021-10-03 20:15:14 -0600

    Added graphics.CloneFromRange and graphics.CloneFromRows.

--- graphics/manipulate.go
+++ graphics/manipulate.go
@@ -8,25 +8,43 @@ import (
 )
 
 // ResizeMaintain resizes img while maintaining its aspect ratio and ensuring that the new image fills the target size.
-// The source image will be cut off in the smaller target dimension if they are not the same aspect ratio.
+// The source image will be cropped in the smaller target dimension if they are not the same aspect ratio.
 // The upper-left corners are aligned.
 // This method uses the Nearest Neighbor interpolation algorithm. For other algorithms, use ResizeMaintainWithInterp.
-func ResizeMaintain(img image.Image, targetWidth, targetHeight uint) image.Image {
+func ResizeMaintain(img SubImager, targetWidth, targetHeight uint) image.Image {
 	return ResizeMaintainWithInterp(img, targetWidth, targetHeight, resize.NearestNeighbor)
 }
 
 // ResizeMaintainWithInterp resizes img while maintaining its aspect ratio and ensuring that the new image fills the target size.
-// The source image will be cut off in the smaller target dimension if they are not the same aspect ratio.
+// The source image will be cropped in the smaller target dimension if they are not the same aspect ratio.
 // The upper-left corners are aligned.
 // The resize is performed using the interpolation algorithm provided by function. Note that resize.NearestNeighbor is
 // the fastest available algorithm, but will not always produce clean results.
-func ResizeMaintainWithInterp(img image.Image, targetWidth, targetHeight uint, function resize.InterpolationFunction) image.Image {
-	// Todo: I think this check is only sufficient if img height is larger than target. Does it just need to be reversed if not, or is it more complex than that (dependent on which width is larger as well)?
+func ResizeMaintainWithInterp(img SubImager, targetWidth, targetHeight uint, function resize.InterpolationFunction) image.Image {
+	// Todo: the logic for which dimension to preserve is fucky. E.g. if the second condition of the second if is changed to float division, it breaks on BlueMoon
+	var r image.Image
+	var w, h uint
 	if targetWidth >= targetHeight && img.Bounds().Dx() >= img.Bounds().Dy() {
-		return resize.Resize(0, uint(targetHeight), img, function)
+		if float32(img.Bounds().Dy())/float32(targetHeight) <= float32(img.Bounds().Dx())/float32(targetWidth) &&
+			img.Bounds().Dx()/img.Bounds().Dy() <= int(targetWidth/targetHeight) {
+			w = 0
+			h = targetHeight
+		} else {
+			w = targetWidth
+			h = 0
+		}
 	} else {
-		return resize.Resize(uint(targetWidth), 0, img, function)
+		if float32(img.Bounds().Dx())/float32(targetWidth) <= float32(img.Bounds().Dy())/float32(targetHeight) &&
+			img.Bounds().Dy()/img.Bounds().Dx() <= int(targetHeight/targetWidth) {
+			w = targetWidth
+			h = 0
+		} else {
+			w = 0
+			h = targetHeight
+		}
 	}
+	r = resize.Resize(w, h, img, function)
+	return r.(SubImager).SubImage(image.Rectangle{Max: image.Point{X: int(targetWidth), Y: int(targetHeight)}})
 }
 
 // MultiplyRect resizes (multiplies) r by factor. r.Min will remain the same. The new r.Size() will be r.Size().Mul(factor).
@@ -44,4 +62,33 @@ func MultiplyRect(r image.Rectangle, factor float64) image.Rectangle {
 	n.Max.Y = int(math.Round(float64(n.Max.Y) * factor))
 
 	return n.Add(r.Min)
+}
+
+// CloneFrom clones the pixel data from src into img.
+// There will be unexpected results if the fields of the Images don't match or the underlying image.Image Color Models
+// don't match (in that case, use draw.Draw).
+func (img *Image) CloneFrom(src *Image) {
+	l := copy(img.Pix, src.Pix)
+	img.Pix = img.Pix[0:l:l]
+}
+
+// CloneFromRange clones the pixel data from src into img, in the range [from,to). It will panic if from or to are
+// < 0 or > the length of either Image's Pix slice or from > to.
+// There will be unexpected results if the fields of the Images (particularly Bounds().Size() or Stride) don't match
+// or the underlying image.Image Color Models don't match (in that case, use draw.Draw).
+// Todo: Untested
+func (img *Image) CloneFromRange(src *Image, from, to int) {
+	copy(img.Pix[from:to], src.Pix[from:to])
+}
+
+// CloneFromRows clones the pixel data from src into img, for the rows (y values) in the range [from,to]. It will panic
+// if from or to are < 0 or >= Bounds().Size().Y or from > to.
+// There will be unexpected results if the fields of the Images (particularly Bounds().Size() or Stride) don't match
+// or the underlying image.Image Color Models don't match (in that case, use draw.Draw).
+// Todo: Untested
+func (img *Image) CloneFromRows(src *Image, from, to int) {
+	start := src.PixOffset(0, from)
+	// The start of the row after to is the (unincluded) end index of the slice to cpy
+	end := src.PixOffset(0, to+1)
+	copy(img.Pix[start:end], src.Pix[start:end])
 }
