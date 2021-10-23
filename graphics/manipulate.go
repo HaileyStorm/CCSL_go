d9f4deefba1ebecc7161c090e6501827edc3ed24
HaileyStorm <HaileyCollet@gmail.com>
2021-10-23 14:29:08 -0600

    Added graphics/Image.CloneFromRect and PlaceAtPoint

--- graphics/manipulate.go
+++ graphics/manipulate.go
@@ -72,23 +72,54 @@ func (img *Image) CloneFrom(src *Image) {
 	img.Pix = img.Pix[0:l:l]
 }
 
-// CloneFromRange clones the pixel data from src into img, in the range [from,to). It will panic if from or to are
-// < 0 or > the length of either Image's Pix slice or from > to.
+// CloneFromRange clones the pixel data from src into img, in the range [from,to).
+// It will panic if from or to are < 0 or > the length of either Image's Pix slice or from > to.
 // There will be unexpected results if the fields of the Images (particularly Bounds().Size() or Stride) don't match
 // or the underlying image.Image Color Models don't match (in that case, use draw.Draw).
-// Todo: Untested
 func (img *Image) CloneFromRange(src *Image, from, to int) {
 	copy(img.Pix[from:to], src.Pix[from:to])
 }
 
-// CloneFromRows clones the pixel data from src into img, for the rows (y values) in the range [from,to]. It will panic
-// if from or to are < 0 or >= Bounds().Size().Y or from > to.
+// CloneFromRows clones the pixel data from src into img, for the rows (y values) in the range [from,to].
+// It will panic if from or to are < 0 or >= Bounds().Size().Y or from > to.
 // There will be unexpected results if the fields of the Images (particularly Bounds().Size() or Stride) don't match
 // or the underlying image.Image Color Models don't match (in that case, use draw.Draw).
-// Todo: Untested
 func (img *Image) CloneFromRows(src *Image, from, to int) {
 	start := src.PixOffset(0, from)
 	// The start of the row after to is the (unincluded) end index of the slice to cpy
 	end := src.PixOffset(0, to+1)
 	copy(img.Pix[start:end], src.Pix[start:end])
+}
+
+// CloneFromRect clones the pixel data within rect (pixel coordinates) from src into img.
+// It will panic if rect is not fully contained by both img and src (their Bounds().Size(), not their Bounds()).
+// The images are assumed to be the same size; the rect is shared between images.
+// It is equivalent to draw.Draw(img, rect, src, rect.Min, draw.Src), but much faster thanks to
+// specific-case optimization.
+// There will be unexpected results if the fields of the Images (particularly Bounds().Size() or Stride) don't match
+// or the underlying image.Image Color Models don't match (in that case, use draw.Draw).
+func (img *Image) CloneFromRect(src *Image, rect image.Rectangle) {
+	var start int
+	dx := rect.Dx() * img.bpp
+	x0 := rect.Min.X * img.bpp
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		start = y*img.Stride + x0
+		copy(img.Pix[start:start+dx], src.Pix[start:start+dx])
+	}
+}
+
+// PlaceAtPoint copies (all of) src onto img at pt on img. It is equivalent to
+// draw.Draw(img, src.Bounds().Sub(src.Bounds().Min), src, image.Point{}, draw.Src), but >50x the speed thanks to
+// specific-case optimization.
+func (img *Image) PlaceAtPoint(src *image.RGBA, pt image.Point) {
+	// Since we know we'll be repeating for each row, calculating the indexes manually with values that don't change
+	// computed only once is about 2x the speed of using PixOffset.
+	var imgStart, srcStart int
+	dx := src.Bounds().Dx() * 4
+	x0 := pt.X * img.bpp
+	for y := 0; y < src.Bounds().Dy(); y++ {
+		imgStart = (pt.Y+y)*img.Stride + x0
+		srcStart = y * src.Stride
+		copy(img.Pix[imgStart:imgStart+dx], src.Pix[srcStart:srcStart+dx])
+	}
 }
